5a9197d12e6e583e5bd6813ff481313f7f9ddfb4
fethulmubin <fetihul@gmail.com>
2025-07-03 21:50:16 +0300

    product addition form created without functionality

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,25 +1,43 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { formatCurrency } from "@/lib/formatters";
 
 const ProductForm = () => {
   return (
     <form action='space-y-4'>
-        <div className="space-y-4">
+        <div className="space-y-2">
             <Label htmlFor="name">Name</Label>
-            <Input type="text" id="name" required/>
+            <Input type="text" id="name"  name="name" required/>
         </div>
 
-         <div className="space-y-4">
-            <Label htmlFor="name">PriceInCents</Label>
-            <Input type="text" id="name" required/>
+         <div className="space-y-2">
+            <Label htmlFor="priceInCents">PriceInCents</Label>
+            <Input type="text"  name="priceInCents" id="priceInCents" required/>
+        </div>
+        <div className="text-muted-foreground">
+            {/* {formatCurrency(priceInCents || 0) / 100} */}
         </div>
 
-         <div className="space-y-4">
-            <Label htmlFor="name">Name</Label>
-            <Input type="text" id="name" required/>
+         <div className="space-y-2">
+            <Label htmlFor="description">Description</Label>
+            <Textarea name="description" id="description" required/>
         </div>
+
+         <div className="space-y-2">
+            <Label htmlFor="file">File</Label>
+            <Input type="file"  name="file" id="file" required/>
+        </div>
+
+         <div className="space-y-2">
+            <Label htmlFor="image">Image</Label>
+            <Input type="file"  name="image" id="image" required/>
+        </div>
+
+        <Button className="mt-2" type="submit">Save</Button>
     </form>
   )
 }
