dd3ab40bd7b13473f4c486df7a434bb99cde91c2
fethulmubin <fetihul@gmail.com>
2025-07-07 13:57:19 +0300

    auth for admin

---
+++ src/lib/isValidPassword.ts
@@ -0,0 +1,13 @@
+export const  isValidPassword = async(password: string,
+     hashedPassword: string ) =>{
+        console.log(await hashPassword(password))
+        return await hashPassword(password) === hashedPassword
+}
+
+async function hashPassword (password: string) {
+    const arrayBuffer = await crypto.subtle.digest(
+        "SHA-256",
+        new TextEncoder().encode(password)
+    );
+    return Buffer.from(arrayBuffer).toString("base64");
+}
